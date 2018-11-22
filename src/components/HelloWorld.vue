06e0898e64b557296ed775d694b1431ea3fd4cfc
quiltacv <quilt@atmarkcafe.org>
2018-11-22 11:12:01 +0700

    wacher

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,8 @@
 <template>
     <div class="my-component">
         <div> {{ message }} - {{ reverseMessage }}</div>
+        <div> Count Click: {{ countClicks }}</div>
+
         <div><button @click="sayHello">Click me</button></div>
     </div>
 </template>
@@ -8,17 +10,36 @@
   export default{
     data() {
       return {
-        message: "FoxZi Vue hello"
+        message: "FoxZi Vue hello",
+        countClicks: 0
       }
     },
+
     computed: {
       reverseMessage() {
         return this.message.split('').reverse().join('')
+      },
+      countClick(){
+        return this.countClicks
       }
     },
+
     methods: {
       sayHello() {
+        this.countClickM()
         return this.message = 'this fun'
+      },
+      countClickM() {
+        return this.countClicks++
+      }
+    },
+
+    watch: {
+      countClick() {
+        console.log("changed count")
+      },
+      message() {
+        console.log("changed messages")
       }
     }
   }
