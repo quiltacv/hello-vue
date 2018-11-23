710b24db7c25b339e6a943ea47c7c569c190a853
quiltacv <quilt@atmarkcafe.org>
2018-11-23 09:54:04 +0700

    show data, set rule console

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,84 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Latest Vue.js Commits</h1>
+    <div v-for="(branch, index) in branches" :key=index>
+      <input type="radio"
+        :id="branch"
+        :value="branch"
+        name="branch"
+        v-model="currentBranch">
+      <label :for="branch"> {{ branch }} </label>
+    </div>
+    <p>vuejs/vue@{{ currentBranch }} </p>
+    <ul>
+      <li v-for="(record, index) in commits" :key=index>
+        <a :href="record.html_url" target="_blank" class="commit">{{ record.sha.slice(0, 7) }}</a>
+        - <span class="message">{{ record.commit.message | truncate }}</span><br>
+        by <span class="author"><a :href="record.author.html_url" target="_blank">{{ record.commit.author.name }}</a></span>
+        at <span class="date">{{ record.commit.author.date | formatDate }}</span>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+var apiURL = 'https://api.github.com/repos/vuejs/vue/commits?per_page=3&sha='
 export default {
   name: 'app',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      branches: ['master', 'dev'],
+      currentBranch: 'master',
+      commits: null
+    }
+  },
+
+  created() {
+    this.fetchData()
+  },
+
+  watch: {
+    currentBranch: 'fetchData'
+  },
+
+  filters: {
+    truncate(v) {
+      var newline = v.indexOf('\n')
+      return newline > 0 ? v.slice(0, newline) : v
+    },
+    formatDate(v) {
+      return v.replace(/TIZ/g, ' ')
+    }
+  },
+
+  methods: {
+    fetchData() {
+      var xhr = new XMLHttpRequest()
+      var self = this
+      xhr.open('GET', apiURL + self.currentBranch)
+      xhr.onload = function () {
+        self.commits = JSON.parse(xhr.responseText)
+        console.log(self.commits[0].html_url)
+      }
+      xhr.send()
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: 'Helvetica', Arial, sans-serif;
+  }
+  a {
+    text-decoration: none;
+    color: #f66;
+  }
+  li {
+    line-height: 1.5em;
+    margin-bottom: 20px;
+  }
+  .author, .date {
+    font-weight: bold;
+  }
 </style>
