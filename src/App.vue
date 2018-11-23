8f5603c57c2637c5bb2e09a66084e7fe38fc0248
quiltacv <quilt@atmarkcafe.org>
2018-11-23 09:23:47 +0700

    add comment

--- src/App.vue
+++ src/App.vue
@@ -17,14 +17,49 @@ export default {
     }
   },
   computed: {
-    compiledMarkdown() {
+    //chuyen các ký tự đạt biết về -
+    compiledMarkdown () {
       return marked(this.input, { sanitize: true })
     }
   },
   methods: {
+    //set deplay test view
     update: _.debounce(function (e) {
       this.input = e.target.value
-    }, 300)
+    }, 1000)
   }
 }
 </script>
+
+<style>
+  html, body, #map {
+    margin: 0;
+    height: 100%;
+    font-family: 'Helvetica Neue', Arial, sans-serif;
+    color: #333;
+  }
+
+  textarea, #map div {
+    display: inline-block;
+    width: 49%;
+    height: 100%;
+    vertical-align: top;
+    box-sizing: border-box;
+    padding: 0 20px;
+  }
+
+  textarea {
+    border: none;
+    border-right: 1px solid #ccc;
+    resize: none;
+    outline: none;
+    background-color: #f6f6f6;
+    font-size: 14px;
+    font-family: 'Monaco', courier, monospace;
+    padding: 20px;
+  }
+
+  code {
+    color: #f66;
+  }
+</style>
