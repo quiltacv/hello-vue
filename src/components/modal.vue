6d0371ac91f4aab1efbb392521c51d391dec6eec
quiltacv <quilt@atmarkcafe.org>
2018-11-26 10:52:13 +0700

    modal

---
+++ src/components/modal.vue
@@ -0,0 +1,37 @@
+<template>
+  <modal>
+    <transition name="modal">
+      <div class="modal-mask">
+        <div class="modal-wrapper">
+          <div class="modal-container">
+
+            <div class="modal-header">
+              <slot name="header">
+                default header
+              </slot>
+            </div>
+
+            <div class="modal-body">
+              <slot name="body">
+                default body
+              </slot>
+            </div>
+
+            <div class="modal-footer">
+              <slot name="footer">
+                default footer
+                <button class="modal-default-button" @click="$emit('close')">
+                  OK
+                </button>
+              </slot>
+            </div>
+          </div>
+        </div>
+      </div>
+    </transition>
+    <h3 slot="header">custom header</h3>
+  </modal>
+</template>
+<script>
+  export default{}
+</script>
