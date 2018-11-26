2417390123e21f2ca56c9849ba0d3ced7cbaa15c
quiltacv <quilt@atmarkcafe.org>
2018-11-26 16:21:16 +0700

    add component event, record

---
+++ src/components/record.vue
@@ -0,0 +1,39 @@
+<template id="record-template">
+  <div class="group realtimedb">
+    <h2>Realtime Datastore</h2>
+    <div class="input-group half left">
+      <label>Firstname</label>
+      <input type="text" v-model="firstname" @input="handleFirstNameUpdate()" />
+    </div>
+    <div class="input-group half">
+      <label>Lastname</label>
+      <input type="text" v-model="lastname" @input="handleLastNameUpdate()" />
+    </div>
+  </div>
+</template>
+<script>
+  export default{
+    props: ['ds'],
+    data() {
+      return {
+        firstname: '',
+        lastname: '',
+      }
+    },
+    created() {
+      this.record = this.ds.record.getRecord('test/johndoe')
+      this.record.subscribe(values => {
+        this.firstname = values.firstname,
+        this.lastname = values.lastname
+      })
+    },
+    methods: {
+      handleFirstNameUpdate() {
+        this.record.set('firstname', this.firstname)
+      },
+      handleLastNameUpdate() {
+        this.record.set('lastname', this.lastname)
+      }
+    }
+  }
+</script>
